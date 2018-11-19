e540eda71c40450c59b48daf51826bbbfe62ff24
GLENNBOT\Glenn <enricoespina@hotmail.com>
2018-11-18 22:11:04 -0600

    Sprint-0/00-Initializations - Adding the ACTUAL Angular Front End to be used. TODO - DELETE the MvcFullSkeleton project/Directory

---
+++ AngularFrontEnd/site/app/example/example.service.ts
@@ -0,0 +1,47 @@
+﻿import { Injectable } from '@angular/core';
+import { IExample } from './example';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/Observable/throw';
+//import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ExampleService {
+
+    constructor(private _http: Http) { }
+
+    getExamples(): Observable<IExample[]> {
+        return this._http.get('http://localhost:42917/examples')
+            .map((response: Response) => <IExample[]>response.json())
+            .catch(this.handleError);
+    }
+
+    getExampleById(exId: string): Observable<IExample> {
+        return this._http.get('http://localhost:42917/examples/' + exId)
+            .map((response: Response) => <IExample>response.json())
+            .catch(this.handleError);
+    }
+
+    handleError(error: Response) {
+        //Change this to pass the exception to some logging service
+        console.error(error);
+        return Observable.throw(error);
+    }
+
+    ////Promise technique
+    //getExampleById(exId: string): Promise<IExample> {
+    //    return this._http.get('http://localhost:42917/examples/' + exId)
+    //        .map((response: Response) => <IExample>response.json())
+    //        .toPromise()
+    //        .catch(this.handlePromiseError);
+    //}
+
+    ////Promise technique
+    //handlePromiseError(error: Response) {
+    //    //Change this to pass the exception to some logging service
+    //    console.error(error);
+    //    throw (error);
+    //}
+}
---
+++ AngularFrontEnd/site/app/example/exampleCount.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+@Component({
+    selector: 'example-count',
+    templateUrl: 'app/example/exampleCount.component.html',
+    styleUrls: ['app/example/exampleCount.component.css']
+})
+export class ExampleCountComponent {
+
+    selectedRadioButtonValue: string = 'All';
+
+    @Output()
+    countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+    @Input()
+    all: number;
+
+    @Input()
+    justOne: number;
+
+    @Input()
+    lessThanOne: number;
+
+    onRadioButtonSelectionChanged() {
+        this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
+    }
+}
