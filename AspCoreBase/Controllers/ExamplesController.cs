d721d3cc640bebe47112a9b255ba8f774a2f3c31
GLENNBOT\Glenn <enricoespina@hotmail.com>
2018-11-19 20:19:40 -0600

    Sprint-0/00-Initializations - Implementing the logic to begin the carrying over of the mvc controllers to API controllers by attempting to implement an EXAMPLE Controller similar to the one in the other repo that I created which involved a regular dot.net base.

---
+++ AspCoreBase/Controllers/ExamplesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreBase.Controllers
+{
+	public class ExamplesController// : ApiController
+	{
+		//public IEnumerable<Example> Get()
+		//{
+		//	using (var exampleEntities = new ExampleDbEntities())
+		//	{
+		//		return exampleEntities.Examples.ToList();
+		//	}
+		//}
+
+		//public Example Get(string par)
+		//{
+		//	using (var exampleEntities = new ExampleDbEntities())
+		//	{
+		//		if (Int32.TryParse(par, out int n))
+		//		{
+		//			return exampleEntities.Examples.FirstOrDefault(e => e.exampleId == n);
+		//		}
+		//		return exampleEntities.Examples.FirstOrDefault(e => e.exampleString == par);
+		//	}
+		//}
+	}
+}
