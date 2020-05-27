1bbf62d716d5700dc03f3ea510a0b71d03338f21
GLENNBOT\Glenn <enricoespina@hotmail.com>
2020-05-26 23:30:57 -0500

    Sprint-04/RHT-06-ReactiveForms - SetValue and patchValue example implementations

--- AngularFrontEnd/site/app/user/ReactiveForm.component.ts
+++ AngularFrontEnd/site/app/user/ReactiveForm.component.ts
@@ -1,5 +1,5 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';    //can also use "FormBuilder", alongside constructor injection, to create a form similar looking to the current manner
 
 @Component({
     selector: 'reactive-form-example',
@@ -23,6 +23,36 @@ export class ReactiveFormComponent implements OnInit {
                 experienceInYears: new FormControl(),
                 proficiency: new FormControl()
             })
+        });
+    }
+
+    //This is just fake data that exists so I don;t have to sample data from the database (or any persisted information)
+    loadFakeDataClick(): void {
+        this.reactiveFormGroup.setValue({   //  "setValue" would be useful for setting data loaded from some other material
+            firstName: 'FakeFirstname',
+            lastName: 'FakeLastName',
+            userName: 'FakeUserName',
+            email: 'fake@email.com',
+            password: 'FakePassword',
+
+            //Nested Form Group Examples (not yet persisted in any kind of memory)
+            skills: {
+                skillName: 'FakeSkill',
+                experienceInYears: 1234,
+                proficiency: 'advanced'
+            }
+        });
+    }
+
+    //This is the PATCH version that would NOT include the nested values. If you used the setValue in the "loadFakeDataClick()" function,
+        //without the nested values, it will complain about not having the nested elements included
+    loadFakeDataPatchClick(): void {
+        this.reactiveFormGroup.patchValue({
+            firstName: 'FakeFirstname',
+            lastName: 'FakeLastName',
+            userName: 'FakeUserName',
+            email: 'fake@email.com',
+            password: 'FakePassword'
         });
     }
 
